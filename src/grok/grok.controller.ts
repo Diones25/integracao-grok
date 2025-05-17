58dd424e0518de69e075ded60896f5925d7fb2f2
Diones <pereiradiones987@gmail.com>
2025-05-17 15:11:24 -0300

    criando controller

--- src/grok/grok.controller.ts
+++ src/grok/grok.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { GrokService } from './grok.service';
+import { PromptDeepSeekDto } from './dtos/prompt-deepseek.dto';
 
 @Controller('grok')
-export class GrokController {}
+export class GrokController {
+
+  constructor(private readonly grokService: GrokService) { }
+  
+  @Post('send')
+  async sendMessage(@Body() promptGrokDto: PromptDeepSeekDto) {
+    const response = await this.grokService.getGrokResponse(promptGrokDto.prompt);
+    return { response }
+  }
+}
